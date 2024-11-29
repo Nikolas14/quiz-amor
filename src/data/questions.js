370fab02191c6bf4f841577bf3c4291e079952c6
Nikolas Ohana <Nikolas14@users.noreply.github.com>
2024-11-29 09:04:28 -0300

    "Updated questions.js: added new questions, options, and answers, and removed empty questions."

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,30 +1,54 @@
 const data = [
-    {
-      question: "O Lider da quintino é nosso Lider pq?",
-      options: ["Pq temos o cartão liderzam", "La tem o melhor sushi da cidade", "Pq no nossos primeiros momentos foram lá"],
-      answer: "Pq no nossos primeiros momentos foram lá",
-      img:"/img/1.jpg",
-    },{
-      question: "",
-      options: ["1", "2", "3"],
-      answer: "1",
-      img:"/img/2.jpg",
-    },{
-      question: "Oq mais faz perdermos o fio da meada da nossa conversa?",
-      options: ["Uma Saveiro roxa", "Ouvir fofoca da mesa do lado", "Uma Hilux verde mosca"],
-      answer: "Uma Saveiro roxa",
-      img:"/img/3.jpg",
-    },{
-      question: "",
-      options: ["1", "2", "Quando a garça só caiu morta do céu"],
-      answer: "Quando a garça só caiu morta do céu",
-      img:"/img/5.jpg",
-    },{
-      question: "Quanto tempo demora uma conversa nossa por videocall?",
-      options: ["5 min", "O tempo passa diferente quando estamos juntos", "13 min"],
-      answer: "O tempo passa diferente quando estamos juntos",
-      img:"/img/4.jpg",
-    },
+  {
+    question: "O líder da Quintino é o nosso líder por quê?",
+    options: [
+      "Porque temos o cartão Liderzam.",
+      "Porque lá tem o melhor sushi da cidade.",
+      "Porque nossos primeiros momentos foram lá."
+    ],
+    answer: "Porque nossos primeiros momentos foram lá.",
+    img: "/img/1.jpg",
+  },
+  {
+    question: "Que você é uma pessoa especial, todos já sabem, mas o que realmente faz a diferença?",
+    options: [
+      "Saber fazer churrasco.",
+      "Ser uma excelente arquiteta.",
+      "Cuidado com a família, construção judaica e gostar de milho assado."
+    ],
+    answer: "Cuidado com a família, construção judaica e gostar de milho assado.",
+    img: "/img/2.jpg",
+  },
+  {
+    question: "O que mais faz perdermos o fio da meada da nossa conversa?",
+    options: [
+      "Uma Saveiro roxa.",
+      "Ouvir fofoca da mesa ao lado.",
+      "Uma Hilux verde mosca."
+    ],
+    answer: "Uma Saveiro roxa.",
+    img: "/img/3.jpg",
+  },
+  {
+    question: "Quanto tempo demora uma conversa nossa por vídeo chamada?",
+    options: [
+      "5 minutos.",
+      "O tempo passa diferente quando estamos juntos.",
+      "13 minutos."
+    ],
+    answer: "O tempo passa diferente quando estamos juntos.",
+    img: "/img/4.jpg",
+  },
+  {
+    question: "Qual foi a cena mais inesperada que vimos juntos?",
+    options: [
+      "Garça caindo morta do céu.",
+      "Um monte de gente pulando em uma carrocinha quebrada do Eder Mauro.",
+      "Keane Reeves da shoppe na academia."
+    ],
+    answer: "O tempo passa diferente quando estamos juntos.",
+    img: "/img/5.jpg",
+  }
     
   ];
   
