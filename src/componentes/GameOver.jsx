67fc022011fe35a55a8c22862cdbce5a9227a704
Nikolas Ohana <Nikolas14@users.noreply.github.com>
2024-11-29 08:48:15 -0300

    "Added WhatsApp share button to GameOver component and updated button styles in index.css"

--- src/componentes/GameOver.jsx
+++ src/componentes/GameOver.jsx
@@ -9,13 +9,35 @@ const GameOver = () => {
 
     const [quizState, dispatch] = useContext(QuizContext)
 
+    const phoneNumber = "5591985598030"; // Substitua pelo número desejado
+    const message = "Vamos fazer nossa casa feliz e kasher!"; // Mensagem opcional
+
+    const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+
   return (
     <div id='gameoverConntainer'>
         <h2>Fim de jogo!</h2>
         <p>Pontuação: {quizState.score}</p>
         <p>Você acertou {quizState.score} de {quizState.questions.length} questões!</p>
         <img src={Welldone} alt='Fim de jogo'></img>
-        <button onClick={()=> dispatch({type:'NEW_GAME'})}>Novo jogo</button>
+        {/* <button onClick={()=> dispatch({type:'NEW_GAME'})}>Novo jogo</button> */}
+        <a
+          href={whatsappLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{
+            display: "inline-block",
+            backgroundColor: "#6BA368",
+            color: "white",
+            padding: "10px 20px",
+            borderRadius: "5px",
+            textDecoration: "none",
+            fontWeight: "bold",
+            textAlign: "center",
+          }}
+        >
+          Vamos fazer nossa casa feliz e kasher?
+        </a>
     </div>
   )
 }
